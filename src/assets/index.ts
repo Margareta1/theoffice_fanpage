bbc7196dcb29371120def4ae9b7078f8938e6741
Margareta <margareta.zeko@gmail.com>
2024-11-10 16:12:38 +0100

    finished navbar and home page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,15 @@
 import { default as TheOfficeLogo } from "./the_office_logo.jpg";
 import { default as TheOfficeCover } from "./the_office_cover.jpg";
 import { default as TheOfficeBackground } from "./the_office_background.jpg";
+import { default as TheOfficeBackground2 } from "./the_office_background2.jpg";
+import { default as TheOfficeMSQuote } from "./the_office_ms_quote.jpg";
+import { default as TheOfficeCollage } from "./the_office_collage.png";
 
-export { TheOfficeLogo, TheOfficeCover, TheOfficeBackground };
+export {
+  TheOfficeLogo,
+  TheOfficeCover,
+  TheOfficeBackground,
+  TheOfficeBackground2,
+  TheOfficeMSQuote,
+  TheOfficeCollage
+};
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,9 +1,14 @@
 import { Breadcrumb } from "antd"
 import { ReactNode } from "react"
+import './../styles/Navbar.scss'
 
 export const Navbar = () =>{
-    const breadcrumbItems: {title:ReactNode}[] = [{title: <a href="/home">Home</a>}]
+    const breadcrumbItems: {title:ReactNode}[] = [
+        {title: <a href="/home">Home</a>},
+        {title: <a href="/cast-and-characters">Cast and characters</a>},
+        {title: <a href="/trivia">Trivia</a>},
+        {title: <a href="/quiz">Quiz</a>},
+    ]
 
     return <Breadcrumb items={breadcrumbItems} className="breadcrumbs" />
-
 }
